c6d2dfdf504ffb42c5768ab3f88d3bf4449b18ec
Capablanca4 <44622746+Capablanca4@users.noreply.github.com>
2026-08-19 13:30:34 +0200

    Added Project to test performance of Parser

---
+++ GW2EIParserBenchmark/EvctParserBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using GW2EIEvtcParser;
+using GW2EIEvtcParser.ParserHelpers;
+using GW2EIGW2API;
+
+namespace GW2EIParserBenchmark;
+
+[MemoryDiagnoser]
+public class EvctParserBenchmark
+{
+    [Params("./TestFiles/20241207-011501.zevtc")]
+    public string _testFile;
+    public EvtcParser parser;
+    ParserController parserController = new TestOperationController();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        EvtcParserSettings parserSettings = new(0, 0);
+        GW2APIController apiController = new("./Content/SkillList.json", "./Content/SpecList.json", "./Content/TraitList.json", "./Content/MapList.json");
+        parser = new EvtcParser(parserSettings, apiController);
+    }
+
+    [Benchmark]
+    public ParsedEvtcLog? ParseLog()
+    {
+        FileInfo fileInfo = new(_testFile);
+        ParsedEvtcLog? test = parser.ParseLog(parserController, fileInfo, out ParsingFailureReason? parsingFailureReasure);
+        return test;
+    }
+}
---
+++ GW2EIParserBenchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using GW2EIParserBenchmark;
+
+BenchmarkRunner.Run<EvctParserBenchmark>();
