81e1f1ed2c6aeb6920afb1cc3c8a39783690e20e
Capablanca4 <44622746+Capablanca4@users.noreply.github.com>
2026-08-20 22:52:55 +0200

    Added Benchmark for GW2APIController

--- GW2EIParserBenchmark/Program.cs
+++ GW2EIParserBenchmark/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BenchmarkDotNet.Running;
 using GW2EIParserBenchmark;
 
-BenchmarkRunner.Run<EvctParserBenchmark>();
+//BenchmarkRunner.Run<EvctParserBenchmark>();
+BenchmarkRunner.Run<GW2APIControllerBenchmark>();
