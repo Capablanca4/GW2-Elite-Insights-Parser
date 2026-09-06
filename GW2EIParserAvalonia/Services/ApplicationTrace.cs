9ce2b4cdee9e90f6d0a3bdb5d4ab11e107e8b943
Linkaaaaa <christian.pagliarani@gmail.com>
2026-09-06 11:44:34 +0200

    Fix crash when writing application traces while parsing multiple logs and added timestamp to each message

--- GW2EIParserAvalonia/Services/ApplicationTrace.cs
+++ GW2EIParserAvalonia/Services/ApplicationTrace.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Threading.Tasks;
 using GW2EIParserCommons;
 using GW2EIParserCommons.Properties;
 
@@ -8,24 +10,28 @@ namespace GW2EIParserAvalonia.Services;
 public sealed class ApplicationTrace : IApplicationTrace
 {
     private readonly string _traceFileName;
+    private readonly BlockingCollection<string> _messages = [];
 
     public ApplicationTrace()
     {
         _traceFileName = $"{ProgramHelper.EILogPath}EILogs-{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
+        _ = Task.Run(WriteToFile);
     }
 
     public void Add(string message)
     {
-        if (!Settings.Default.ApplicationTraces)
+        if (Settings.Default.ApplicationTraces)
         {
-            return;
+            _messages.Add($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}] {message}");
         }
+    }
 
-        if (!Directory.Exists(ProgramHelper.EILogPath))
+    private void WriteToFile()
+    {
+        foreach (string message in _messages.GetConsumingEnumerable())
         {
             Directory.CreateDirectory(ProgramHelper.EILogPath);
+            File.AppendAllText(_traceFileName, message + Environment.NewLine);
         }
-
-        File.AppendAllText(_traceFileName, message + Environment.NewLine);
     }
 }
